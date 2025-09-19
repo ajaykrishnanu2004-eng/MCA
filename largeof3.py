3d0ae6bc8275a64d30ed168a9631a41511d99089
ajaykrishnanu2004-eng <ajaykrishnanu2004@gmail.com>
2025-09-19 11:23:14 +0530

    Add files via upload

---
+++ largeof3.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+c=int(input("enter the third number:"))
+if a>b:
+   print("largest number is:",a)
+if b>c:
+   print("largest number is:",b)
+else:
+     print("largest number is:",c)              
