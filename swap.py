d89542440eda8c66873b12da37e438b31fdb2013
ajaykrishnanu2004-eng <ajaykrishnanu2004@gmail.com>
2025-09-19 11:22:07 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,22 @@
+a=int(input("enter the first number(a):"))
+b=int(input("enter the second number(b):"))
+print("before swap")
+print("a",a)
+print("b",b)
+temp=a
+a=b
+b=temp
+print("after swap")
+print("a",a)
+print("b",b)
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+print("before swap")
+print("x=",x)
+print("y=",y)
+x=x+y
+y=x-y
+x=x-y
+print("after swap")
+print("x=",x)
+print("y=",y)
